006c1156275b2e2b864fc4e6085aad49d479aa5a
Alexandra Stavreva <alexastavreva@gmail.com>
2025-06-02 23:14:31 +0300

    weighted graph

--- pesho/extractFile.c
+++ pesho/extractFile.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "graph.h"
+#include <math.h>
 
 typedef struct Platform
 {
     int x;
     int y;
     int length;
+    int val;
+    int count;
 } Platform;
 
 Platform *findCoordinates(const char *name)
@@ -18,9 +21,11 @@ Platform *findCoordinates(const char *name)
     int currRow = 1, currColumn = 1, startColumn = 1;
 
     // array of platforms of the platforms
-    int length = 0, i = 0, platformsCount = 0, capacity = 10;
+    int length = 0, i = 0, capacity = 10;
     // int *platforms = (int *)malloc(sizeof(int) * capacity);
     Platform *platforms = malloc(sizeof(Platform) * capacity);
+
+    platforms->count = 0;
 
     while ((s = fgetc(file)) != EOF)
     {
@@ -35,15 +40,16 @@ Platform *findCoordinates(const char *name)
         {
             if (length > 0)
             {
-                if (platformsCount == capacity)
+                if (platforms->count == capacity)
                 {
                     capacity *= 2;
                     platforms = realloc(platforms, sizeof(Platform) * capacity);
                 }
-                platforms[platformsCount].length = length;
-                platforms[platformsCount].x = startColumn;
-                platforms[platformsCount].y = currRow;
-                platformsCount++;
+                platforms[platforms->count].val = platforms->count;
+                platforms[platforms->count].length = length;
+                platforms[platforms->count].x = startColumn;
+                platforms[platforms->count].y = currRow;
+                platforms->count++;
                 length = 0;
             }
 
@@ -60,19 +66,20 @@ Platform *findCoordinates(const char *name)
 
     if (length > 0)
     {
-        if (platformsCount == capacity)
+        if (platforms->count == capacity)
         {
             capacity *= 2;
             platforms = realloc(platforms, sizeof(int) * capacity);
         }
-        platforms[platformsCount].length = length;
-        platforms[platformsCount].x = startColumn;
-        platforms[platformsCount].y = currRow;
+        platforms[platforms->count].val = platforms->count;
+        platforms[platforms->count].length = length;
+        platforms[platforms->count].x = startColumn;
+        platforms[platforms->count].y = currRow;
     }
 
     fclose(file);
 
-    for (int i = 0; i < platformsCount; i++)
+    for (int i = 0; i < platforms->count; i++)
     {
         printf("\n%d platform x: %d, y: %d, and is %d long", i, platforms[i].x, platforms[i].y, platforms[i].length);
     }
@@ -80,15 +87,37 @@ Platform *findCoordinates(const char *name)
     return platforms;
 }
 
-int findEdgesWeight(const char *name, int *platforms)
+int findEdgesWeight(const char *name, Platform *platforms, int peshoSteps)
 {
+    FILE *file = fopen(name, "r");
+    int s;
+    while ((s = fgetc(file)) != EOF)
+    {
+        if (s == "#")
+            break;
+    }
+
+    Graph *map;
+    initGraph(platforms->count);
+
+    for (int i = 0; i < platforms->count; i++)
+    {
+        for (int j = 0; j < platforms->count; j++)
+        {
+            int X = abs(platforms[i].x - platforms[j].x);
+            int Y = abs(platforms[j].y - platforms[i].y);
+            int weight = sqrt(pow(X, 2) + pow(Y, 2));
+            addEdge(map, i, j, weight);
+        }
+    }
 }
 
 int main()
 {
-    char *fileName = "map.txt";
+    const char *fileName = "map.txt";
     Platform *platforms;
+    int steps = 5;
 
     platforms = findCoordinates(fileName);
-    findEdgesWeight(fileName, platforms);
+    findEdgesWeight(fileName, platforms, steps);
 }
